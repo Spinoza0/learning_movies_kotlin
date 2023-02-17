416a7766ca8f19687cd6bfe23aa6d9dfcc7835c2
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-02-17 17:36:09 +0300

    bugfix

--- app/src/main/java/com/spinoza/movieskotlin/data/database/MovieDao.kt
+++ app/src/main/java/com/spinoza/movieskotlin/data/database/MovieDao.kt
@@ -9,16 +9,13 @@ import com.spinoza.movieskotlin.data.database.model.MovieDbModel.Companion.TABLE
 @Dao
 interface MovieDao {
     @Query("SELECT * FROM $TABLE_FAVOURITES")
-    fun getAllFavouriteMovies(): List<MovieDbModel>
-
-    @Query("SELECT * FROM $TABLE_FAVOURITES WHERE id=:movieId")
-    fun getFavouriteMovie(movieId: Int): MovieDbModel
+    suspend fun getAllFavouriteMovies(): List<MovieDbModel>
 
     @Insert
-    fun insertMovie(movie: MovieDbModel)
+    suspend fun insertMovie(movie: MovieDbModel)
 
     @Query("DELETE FROM $TABLE_FAVOURITES WHERE id=:movieId")
-    fun removeMovie(movieId: Int)
+    suspend fun removeMovie(movieId: Int)
 
     @Query("SELECT EXISTS (SELECT * FROM $TABLE_FAVOURITES WHERE id=:movieId)")
     suspend fun isMovieFavourite(movieId: Int): Boolean
